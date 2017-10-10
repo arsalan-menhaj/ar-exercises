e3f7ea36f49fb3dc303dcd5560d61a39c9b95822
arsalan-menhaj <arsalan.menhaj@gmail.com>
2017-10-10 19:45:31 +0000

    Added destroy restrction for Store model

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,6 +1,22 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+
+  # The password generator should be registered as a
+  # before_create callback so that, if it fails, then
+  # the entire employee creation process fails. This
+  # is ideal, since we do not want a user to be created
+  # without a password. Similarly, we do not want it to
+  # be a before_save callback, since the save might fail,
+  # leaving inconsistent data.
+  before_create :generate_password
+
+  private
+    def generate_password
+      self.password = (0...8).map { ('a'..'z').to_a[rand(26)] }.join
+    end
 end
+
--- lib/store.rb
+++ lib/store.rb
@@ -5,6 +5,14 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validate :sells_mens_or_womens_apparel
 
+  def destroy
+    if self.employees.all.count > 0
+      return false
+    end
+    super
+  end
+
+
   def sells_mens_or_womens_apparel
     if !mens_apparel && !womens_apparel
       errors.add(:mens_apparel, "must be true if the other category is false, and vice-versa.")
